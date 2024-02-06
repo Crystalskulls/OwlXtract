8e904a6537247501ec1cececd96e7e232ffd095a
Benedikt Müller <bene93.mueller@web.de>
2024-02-06 21:33:49 +0100

    ✨ import materialdesignicons as default

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
-const vuetify = createVuetify()
-
+export const vuetify = createVuetify()
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-
 app.mount('#app')
